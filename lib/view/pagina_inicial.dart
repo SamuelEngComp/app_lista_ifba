4ec9fd4da5b4419618a0a8da1a6789f91d6d1a3c
Samuel Lima de Farias <samuel.lima@ebserh.gov.br>
2022-01-12 11:30:40 -0300

    adicionei um SilverAppBar na tela inicial

--- lib/view/pagina_inicial.dart
+++ lib/view/pagina_inicial.dart
@@ -11,22 +11,61 @@ import 'package:flutter_application_1/view/questao09/questao_09_view.dart';
 import 'package:flutter_application_1/view/questao10/questao_10_view.dart';
 
 class PaginaInicial extends StatelessWidget {
+
+  
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> itensTelaPrincipal = [Questao01(),
+            Questao02(),
+            Questao03(),
+            Questao04(),
+            Questao05(),
+            Questao06(),
+            Questao07(),
+            Questao08(),
+            Questao09(),
+            Questao10(),];
+
     return Scaffold(
-      appBar: AppBar(
+      /* appBar: AppBar(
         leading: Icon(Icons.work),
         centerTitle: true,
         title: const Text(
           'Exercicios IFBA',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+      ), */
+      body: CustomScrollView(
+        /* width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height, */
+        slivers: [
+          SliverAppBar(
+            centerTitle: true,
+            pinned: true,
+            floating: true,
+            expandedHeight: 200,
+            stretch: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset('assets/img/logo_ifba.png',
+                  alignment: Alignment.center,
+                  /* fit: BoxFit.cover */),
+              title: Text('Exercícios IFBA'),
+              centerTitle: true,
+              stretchModes: [
+                StretchMode.fadeTitle,
+                StretchMode.zoomBackground,
+                StretchMode.blurBackground,
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(itensTelaPrincipal)),
+        ],
+
+
         //color: Colors.red,
-        child: ListView(
+        /* child: ListView(
           children: [
             Questao01(),
             Questao02(),
@@ -39,7 +78,7 @@ class PaginaInicial extends StatelessWidget {
             Questao09(),
             Questao10(),
           ],
-        ),
+        ), */
       ),
     );
   }
